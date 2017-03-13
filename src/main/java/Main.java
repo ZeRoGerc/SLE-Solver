7fcba4e4de21afa11d1ee76a3e5d8c12f9095ccd
Uladzislau Sazanovich <zerogerc@gmail.com>
2017-03-14 00:35:02 +0300

    initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by zerogerc on 14.03.17.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        SwingLayoutDemo swingLayoutDemo = new SwingLayoutDemo();
+        swingLayoutDemo.showGridLayoutDemo();
+    }
+}
